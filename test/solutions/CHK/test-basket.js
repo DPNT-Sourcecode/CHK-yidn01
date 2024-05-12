2d17ee013f7d789ca330240f443b5ebc7900ed15
sbx_user1051 <sbx_user1051@169.254.89.237>
2024-05-12 16:52:44 +0000

    Sun May 12 16:52:44 UTC 2024

--- test/solutions/CHK/test-basket.js
+++ test/solutions/CHK/test-basket.js
@@ -5,27 +5,12 @@ var assert = require('assert');
 const Basket = require('../../../lib/solutions/CHK/basket');
 
 describe('CHK challenge: basket class', function() {
-	it('should return the price for one A', function() {
-	    assert.equal(checkout('A'), 50);
+	it('should be initializable with valid input', function() {
+	    assert.doesNotThrow(() => new Basket('AAB'));
 	});
 
-    it('should return the price for one A and one B', function() {
-	    assert.equal(checkout('AB'), 80);
-	});
-
-    it('should return the discounted price for three As', function() {
-	    assert.equal(checkout('AAA'), 130);
-	});
-
-    it('should return the discounted price for four As and misc others', function() {
-	    assert.equal(checkout('ABAACA'), 230);
-	});
-
-    it('should return -1 for invalid input', function() {
-	    assert.equal(checkout('E'), -1);
-	});
-
-    it('should return -1 for invalid input at the end of the string', function() {
-	    assert.equal(checkout('ABCAE'), -1);
-	});
+    it('is not empty when initialized with items', function() {
+        const basket = new Basket('AAB')
+        assert.equal(basket.isEmpty(), false)
+    })
 });
