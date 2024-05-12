f8cc2478bdc7f68545b5ba141d277b3dadd01660
sbx_user1051 <sbx_user1051@169.254.89.237>
2024-05-12 17:31:44 +0000

    Sun May 12 17:31:44 UTC 2024

--- test/solutions/CHK/test-checkout.js
+++ test/solutions/CHK/test-checkout.js
@@ -33,6 +33,18 @@ describe('CHK challenge: supermarket checkout', function() {
 		assert.equal(checkout('AAAAAA'), 250)
 	})
 
+	it('should give one B for free with two Es', function() {
+		assert.equal(checkout('EEB'), 80)
+	})
+
+	it('should give one B for free with two Es even if 2B discount can be taken', function() {
+		assert.equal(checkout('EEBB'), 110)
+	})
+
+	it('should give two free Bs with four Es', function() {
+		assert.equal(checkout('EEEEBB'), 160)
+	})
+
     it('should return -1 for invalid input', function() {
 	    assert.equal(checkout('#'), -1);
 	});
